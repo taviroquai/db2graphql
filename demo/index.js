b9565d1c9e4445e47926db6f3f865ab4f901dfa8
Marco Afonso <mafonso333@gmail.com>
2019-01-20 17:37:53 +0000

    Update demo

--- demo/index.js
+++ demo/index.js
@@ -1,24 +1,37 @@
-const { start } = require('./server');
-  
-start(({ resolver }) => {
+const knex = require('knex');
+const db2g = require('../src/db2g');
+const { ApolloServer, gql } = require('apollo-server');
 
-  // Example of how to hook your resolver for custom behaviour
-  // API: getPage, getFirstOf and putItem
-  resolver.on('getFirstOf', async (root, args, context) => {
-    const { resolver, tablename, db } = context.ioc;
-    
-    // Use default result
-    const user = await resolver.getFirstOf(tablename, args);
+const start = async (cb) => {
+  const api = new db2g(knex(require('./connection.json')));
+  await api.init(); // Connects to database and load dependencies
 
-    // Transform data
-    delete user.password;
-
-    // Send output
-    console.log('My resolver result', user);
-    return user;
+  // Example of how to add your own queries and resolvers
+  api.addQuery('getFoo: Boolean');
+  api.addResolver('Query', 'getFoo', async (root, args, context) => {
+    return true;
   });
-})
-.catch(err => {
-  console.log(err.message);
-  process.exit();
-})
+
+  // Example on how to override a generated resolver
+  api.override('getFirstOf', async (root, args, context) => {
+    const { resolver, tablename, db } = context.ioc;
+    if (tablename === 'categories') return await db('categories').first();
+    return resolver.getFirstOf(tablename, args);
+  });
+
+  // Create Graphql server
+  const schema = await api.getSchema();
+  console.log(schema); // Show generated schema on run
+  if (!schema) throw new Error('Error: empty schema');
+
+  // Create Apollo Server and start
+  const server = new ApolloServer({
+    typeDefs: gql`${schema}`,
+    resolvers: await api.getResolvers(),
+  });
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+}
+
+start();
