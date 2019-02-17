6ba602ed3948ac90a2a0ecf7e00148517f5ce9fc
Marco Afonso <mafonso333@gmail.com>
2019-02-17 11:40:10 +0000

    Fix builder test

--- test/db2g.test.js
+++ test/db2g.test.js
@@ -166,6 +166,8 @@ type Mutation {
     });
     result = await resolvers.Query.dropSchemaTable(null, { tablename: "foo" }, {});
     expect(result).toEqual(true);
+    result = await resolvers.Query.dropSchemaTable(null, { tablename: "foo" }, {});
+    expect(result).toEqual(false);
 
     await db.schema.dropTableIfExists('foo');
     await db.schema.createTable('foo', (table) => {
